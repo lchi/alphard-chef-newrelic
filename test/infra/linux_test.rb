4e3419ac1e36fd949bbca1bfc9009062791c3ebc
Frédéric Nowak <frederic.nowak@gmail.com>
2017-05-10 17:24:19 +0200

    Fix tests for infra

--- test/infra/linux_test.rb
+++ test/infra/linux_test.rb
@@ -7,14 +7,15 @@
 
 unless os.windows?
 
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+  describe package('newrelic-infra') do
+    it { should be_installed }
+  end
+
+  describe service('newrelic-infra') do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
   end
 
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
-end
